d5d4e1924a087071c19e89f1c29b1330c95ab4ec
EsterAparicio <anseryne@gmail.com>
2018-02-21 11:21:41 +0100

    Add files via upload

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -1,7 +1,8 @@
 window.onload = iniciarFunciones;
 function iniciarFunciones(){
 	var scores = "/file/scores.txt";
-	var f = new File (scores);
+	//var f = new File([], scores);
+	var tiempo = 30;
 
 
 	document.getElementById("user").addEventListener("blur", saveUser);
@@ -11,7 +12,15 @@ function iniciarFunciones(){
 		window.location.href = "index.html";
 	}
 
-	function saveUser(){
+	function temporizador(){
+		for(var i = tiempo; i > 0; i--){
+			setTimeOut(function(){
+				document.getElementById("tiempo").value = i-1;
+			}, 1000);
+		}
+	}
+
+	/*function saveUser(){
 		var user = document.getElementById("user").value;
 		f.open("w");
 		f.writeln(user);
